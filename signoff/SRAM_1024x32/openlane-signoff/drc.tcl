9894a363a28dbcd5991baf4b0675d1051ddf3c47
Passant <bassant.hassan@efabless.com>
2024-05-08 12:33:28 +0300

    update openlane config of `SRAM_1024x32` and reharden it

---
+++ signoff/SRAM_1024x32/openlane-signoff/drc.tcl
@@ -0,0 +1,3 @@
+box 10.100um 233.805um 10.360um 234.065um; feedback add "Metal1 minimum area < 0.083um^2 (met1.6)" medium
+box [INFO]: COUNT: 1; feedback add "Metal1 minimum area < 0.083um^2 (met1.6)" medium
+box [INFO]: Should be divided by 3 or 4; feedback add "Metal1 minimum area < 0.083um^2 (met1.6)" medium
