021e3556991c47adf83ef8c4b579ac772ea931a5
Passant <bassant.hassan@efabless.com>
2024-05-09 12:49:17 +0300

    update `user_rpoject_wrapper` implementation with the latest `SRAM_1024x32` macro

--- signoff/user_project_wrapper/openlane-signoff/drc.tcl
+++ signoff/user_project_wrapper/openlane-signoff/drc.tcl
@@ -1,3 +1,12 @@
 box 25.160um 348.805um 25.420um 349.065um; feedback add "Metal1 minimum area < 0.083um^2 (met1.6)" medium
-box [INFO]: COUNT: 1; feedback add "Metal1 minimum area < 0.083um^2 (met1.6)" medium
+box N-well overlap of Deep N-well < 0.4um outside, 1.03um inside (nwell.5a, 7); feedback add "Metal1 minimum area < 0.083um^2 (met1.6)" medium
+box 25.160um 157.595um 26.590um 314.560um; feedback add "Metal1 minimum area < 0.083um^2 (met1.6)" medium
+box 25.160um 156.165um 314.560um 157.595um; feedback add "Metal1 minimum area < 0.083um^2 (met1.6)" medium
+box 25.160um 540.275um 314.560um 541.705um; feedback add "Metal1 minimum area < 0.083um^2 (met1.6)" medium
+box 25.160um 314.560um 26.590um 540.275um; feedback add "Metal1 minimum area < 0.083um^2 (met1.6)" medium
+box 327.045um 157.595um 328.475um 314.560um; feedback add "Metal1 minimum area < 0.083um^2 (met1.6)" medium
+box 314.560um 156.165um 328.475um 157.595um; feedback add "Metal1 minimum area < 0.083um^2 (met1.6)" medium
+box 314.560um 540.275um 328.475um 541.705um; feedback add "Metal1 minimum area < 0.083um^2 (met1.6)" medium
+box 327.045um 314.560um 328.475um 540.275um; feedback add "Metal1 minimum area < 0.083um^2 (met1.6)" medium
+box [INFO]: COUNT: 9; feedback add "Metal1 minimum area < 0.083um^2 (met1.6)" medium
 box [INFO]: Should be divided by 3 or 4; feedback add "Metal1 minimum area < 0.083um^2 (met1.6)" medium
